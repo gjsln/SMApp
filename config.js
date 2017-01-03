b86db0499089c2b0a71f94b4b59af378e1d2eb09
Gnanavel N <gnanaveln@gmail.com>
2017-01-03 15:31:44 +0530

    Update config.js

--- config.js
+++ config.js
@@ -23,3 +23,57 @@ module.exports = {
     ]
   },
 };
+
+
+/******* Reduce data ****/
+
+var textData =
+    {
+	"id": "0001",
+	"type": "donut",
+	"name": "Cake",
+	"ppu": 0.55,
+	"batters":
+		{
+			"batter":
+				[
+					{ "id": "1001", "type": "Regular" },
+					{ "id": "1002", "type": "Chocolate" },
+					{ "id": "1003", "type": "Blueberry" },
+					{ "id": "1004", "type": "Devil's Food" }
+				]
+		},
+	"topping":
+		[
+			{ "id": "5001", "type": "None" },
+			{ "id": "5002", "type": "Glazed" },
+			{ "id": "5005", "type": "Sugar" },
+			{ "id": "5007", "type": "Powdered Sugar" },
+			{ "id": "5006", "type": "Chocolate with Sprinkles" },
+			{ "id": "5003", "type": "Chocolate" },
+			{ "id": "5004", "type": "Maple" }
+		]
+};
+
+/*
+textData = textData.batters.filter(function(textData) {
+    return textData; // if truthy then keep item
+}).map(function(textData) {
+    return textData;
+});
+
+console.log(JSON.stringify(textData));
+
+*/
+
+textData = textData.batters.batter.reduce(function(memo, textData) {
+    if (textData.id) { // this serves as our `filter`
+        memo.push({ // this serves as our `map`
+            id: textData.id,
+            type: textData.type
+        });
+    }
+    return memo;
+}, []);
+
+console.log(JSON.stringify(textData, null, 4));
